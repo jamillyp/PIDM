c1d081da974d3da6631d5591d55445ff5926da65
Jamilly <jamillyp@alu.ufc.br>
2021-04-04 22:57:49 -0300

    Adicionando funções de Inserção e Exclusão no atv07

--- Trabalho_07/atv07/src/components/Add/index.jsx
+++ Trabalho_07/atv07/src/components/Add/index.jsx
@@ -1,15 +1,23 @@
-import React, { useEffect, useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, TouchableHighlight, TouchableOpacity, View } from 'react-native';
-
+import React, { useCallback, useEffect, useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, TouchableHighlight, View, ScrollView, Modal } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 import { database } from '../../config/firebase';
 
-export default function Add() {
-
+const Add = () => {
   const [ cursos, setCursos ] = useState([]);
   const [ nome, setNome ] = useState('');
   const [ coordenador, setCoordenador ] = useState('');
+  const [state, setState] = useState(false);
+
+  const [setUserType] = useState(null);
+  const getUserType = useCallback(async () => {
+    const response = await AsyncStorage.getItem('@storage_Key');
+    setUserType(response);
+  }, []);
 
   useEffect(() => {
+    getUserType();
+    handleCloseDrawer();
     database.collection('cursos').onSnapshot((query) => {
       const listaCursos = [];
       query.forEach((doc) => {
@@ -17,7 +25,11 @@ export default function Add() {
       })
       setCursos(listaCursos);
     })
-  }, [])
+  },  [getUserType, handleCloseDrawer])
+
+  const handleCloseDrawer = useCallback(() => {
+    setState(false);
+  }, []);
 
   function addCurso() {
     database.collection('cursos').add({
@@ -25,91 +37,146 @@ export default function Add() {
       coordenador
     });
   }
+
+  function deletar(id) {
+    database.collection('cursos').doc(id).delete();
+    setState(false);
+  }
   
   return (
-    <View style={styles.container}>
+    <>
+    {state ? (
+        <Modal>
+          <Text style={{top: 200, marginLeft: 120, fontSize: 20, fontWeight: 'bold'}}>Deseja excluir?</Text>
 
-      <TextInput 
-      style={styles.text}
-      placeholder="nome do curso"  
-      value={ nome } 
-      onChangeText={ setNome } />
+          <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center',}}>
+            <TouchableHighlight title="Não" onPress={() => setState(false)} style={{top: 250, alignItems: 'center', backgroundColor: '#20B2AA', width: 60, height: 40, textAlign: 'center', justifyContent: 'center', marginRight: 20, fontWeight: 'bold'}}>
+              <Text style={{color: 'white'}}>
+                Não
+              </Text>
+            </TouchableHighlight>
 
-      <TextInput 
-      style={styles.text}
-      placeholder="coordenador(a)" 
-      value={ coordenador } 
-      onChangeText={ setCoordenador } />
+            {cursos.map((curso) => {
+              return <TouchableHighlight title="Sim" onPress={() => deletar(curso.id)} style={{top: 250, alignItems: 'center', backgroundColor: '#90EE90', width: 60, height: 40, textAlign: 'center', justifyContent: 'center', fontWeight: 'bold'}}>
+              <Text style={{color: 'white'}}> Sim </Text> 
+            </TouchableHighlight>
+            })}
+          </View>
+        </Modal>
+    ) : (
+      false
+  )}
 
-      <TouchableHighlight
-      title="Inserir" 
-      onPress={ addCurso }
-      style={styles.button}
-      >
-          <Text style={styles.textButton}>
-              Inserir
-          </Text>
-      </TouchableHighlight>
+    <ScrollView
+      contentContainerStyle={{ flex: 1 }}
+      keyboardShouldPersistTaps="handled"
+    >
 
+      <View style={styles.container}>
 
-      {cursos.map((curso) => {
-        return <View style={styles.viewButton}> 
-        <Text key={ curso.id }> { curso.nome } - { curso.coordenador }</Text>
-        <TouchableHighlight style={styles.button2} title='Excluir'>
+        <TextInput 
+        style={styles.textInput}
+        label="qual o nome do curso?"
+        placeholder="qual o nome do curso?"  
+        value={ nome } 
+        onChangeText={ setNome } />
+
+        <TextInput 
+        style={styles.textInput}
+        label="qual o(a) coordenador(a)?"
+        placeholder="qual o(a) coordenador(a)?" 
+        value={ coordenador } 
+        onChangeText={ setCoordenador } />
+
+        <TouchableHighlight
+        title="Inserir" 
+        onPress={ addCurso }
+        style={styles.button}
+        >
             <Text style={styles.textButton}>
-                Excluir
+                SALVAR
             </Text>
         </TouchableHighlight>
-        <TouchableHighlight style={styles.button2} title='Excluir'>
-            <Text style={styles.textButton}>
-                Editar
-            </Text>
-        </TouchableHighlight>
-        </View>
-        
-      })}
 
-    </View>
+        {cursos.map((curso) => {
+          return <View style={styles.view}>
+
+            <View style={styles.viewButton}>
+              <Text key={ curso.id } style={styles.text}> { curso.nome } - { curso.coordenador }</Text>
+            </View>
+
+            <View style={styles.viewButton}>
+              <TouchableHighlight style={styles.button2} title='Excluir' onPress={() => setState(true)}>
+                <Text style={styles.textButton}>
+                  <Feather name="x" size={30} color="#FF6961" />
+                </Text>
+              </TouchableHighlight>
+              <TouchableHighlight style={styles.button2} title='Editar'>
+                <Text style={styles.textButton}>
+                <Feather name="edit" size={30} color="#FDFD96" />
+                </Text>
+              </TouchableHighlight>
+            </View>
+
+          </View>
+          
+        })}
+
+      </View>
+    </ScrollView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    backgroundColor: '#20B2AA',
+  },
+  textInput: {
+    paddingLeft: 10,
+    width: '80%',
+    height: 50,
+    marginBottom: 10,
+    fontSize: 16,
+    backgroundColor: 'white'
   },
   text: {
-      width: '80%',
-      height: 50,
-      borderRadius: 5,
-      fontSize: 16
+    color: 'white'
   },
   button: {
     justifyContent: 'center',
-     width: '80%',
-     height: '8%',
-     backgroundColor: 'black'
+    width: '80%',
+    height: '8%',
+    backgroundColor: '#90EE90'
   },
   viewButton: {
-      top: 10,
-      flexDirection: 'row',
-      marginBottom: 10,
-      width: '80%',
-      height: '5%',
-      justifyContent: 'space-between',
-      alignItems: 'center',
+    marginBottom: 10,
+    flexDirection: 'row',
+    width: '50%',
+    justifyContent: 'space-around',
+    paddingLeft: 10
   },
   button2: {
     justifyContent: 'center',
-     backgroundColor: 'grey',
-     borderRadius: 5
   },
   textButton:{
-      fontWeight: 'bold',
-      fontSize: 15,
-      textAlign: 'center',
-      color: 'white',
+    fontWeight: 'bold',
+    fontSize: 15,
+    textAlign: 'center',
+    color: 'white',
+  },
+  view: {
+    flexDirection: 'row',
+    width: '80%',
+    top: 15,
+    marginBottom: 10,
+    paddingBottom: 5,
+
   }
 });
+
+export default Add;
